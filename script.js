4cfd76cee3e884004af85928746ccec8dcdd0047
Vagner Souza <66963813+vagnersouza23@users.noreply.github.com>
2023-10-25 19:55:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
 const containerVideos = document.querySelector('.videos__container')
 
-const api = fetch('http://localhost:3000/videos')
-.then(res => res.json())
-.then((videos) => 
+async function buscarEMostrarVideos(){
+    const busca = await fetch('http://localhost:3000/videos')
+    const videos = await busca.json()
     videos.forEach((video) => {
         containerVideos.innerHTML += `
         <li class="videos__item">
@@ -12,16 +13,12 @@ const api = fetch('http://localhost:3000/videos')
                 <h3 class='titulo-video'>${video.titulo}</h3>
                 <p class='titulo-canal'>${video.descricao}</p>
             </div>
-
         </li>
         `
     })
-)
-.catch((error) => {
-    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}<p>`
-})
+}
 
-
+buscarEMostrarVideos()
 
 
 
